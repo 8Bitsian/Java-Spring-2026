cee2274ac933361bce53f09ff5fc634efdf5e4ca
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-01-20 23:06:09 -0500

    Create J104_2.java

---
+++ lesson04/J104_2.java
@@ -0,0 +1,35 @@
+/*
+   Imani Hollie 01/20/2026
+   Lesson 4 Lab: Convert a positive number as either days or seconds:
+   - If user selects Day (1), the input represents days
+     The program converts the given days into years, months and days, assuming:
+     - 1 year = 365 days
+     - 1 month = 30 days
+   - If user selects Time (2), the input represents seconds
+     The program converts the given days into hours, minutes and seconds, assuming:
+     - 1 hour = 3600 seconds
+     - 1 minute = 60 seconds
+     
+   Sample Output:
+   Day (1) or Time (2)? 1
+   ? 123456789
+   Your day is *** years *** months and *** days
+
+   Day (1) or Time (2)? 2
+   ? 123456789
+   Your day is *** hours *** minutes and *** seconds
+*/
+
+import java.util.Scanner;  // Allows use of Scanner objects
+public class J104_2 {      // J104_2 Class
+  public static void main(String[] args) {  // Program enters here
+    Scanner input = new Scanner(System.in);  // Create Scanner object 'input'
+    // Program logic
+    // Declare constants
+    
+    // Declare variables
+    
+    // Program logic
+    input.close();  // Close 'input'
+  }
+}  // End of program
