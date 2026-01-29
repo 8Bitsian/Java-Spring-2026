7847479e6cec9ea7a60ab8ea62307d4abf6bf7d1
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-01-29 18:52:34 -0500

    Add ComputeArea class to calculate circle area

---
+++ lesson02/ComputerArea.java
@@ -0,0 +1,25 @@
+// Import libraries
+import java.util.Scanner;                       // Allows use of Scanner objects
+
+public class ComputeArea {                      // Class 'ComputerArea'
+    public static void main(String[] args) {    // Method 'main' (Entry Point)
+        // Create objects
+        Scanner input = new Scanner(System.in); // Create Scanner object 'input'
+
+        // Declare constants
+        // final double PI = 3.14159;           // Declare constant w/final and name all caps
+
+        // Get user input
+        System.out.print("Enter Radius: ");     // Output prompt to console
+        double radius = input.nextDouble();     // Declare variable 'radius'
+
+        // Calculate the area of a circle
+        double area = Math.pow(radius, 2) * Math.PI; // Declare variable 'area' w/Math.pow() and Math.PI
+
+        // Output area of circle
+        System.out.println("The area for " + radius + " is: " + area);
+
+        // Close Scanner object 'input'
+        input.close();
+    }
+}   // Exit program
