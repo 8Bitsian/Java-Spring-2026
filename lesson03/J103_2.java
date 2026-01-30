d18637abde9adac68f6a89246f7b2434549c43a7
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-01-29 20:19:17 -0500

    Implement BMI calculation and remove unused code
    Removed unused code related to odd/even number check and added BMI calculation and output.

--- lesson03/J103_2.java
+++ lesson03/J103_2.java
@@ -31,25 +31,35 @@
    Your BMI is *** which is ***
 */
 
-import java.util.Scanner;
+// Part 1 of Lab Project
+public class J103_2 {
+  public static void main(String[] args) {
+    // Define Constants
+    final double KILOGRAMS_PER_POUND = 0.4535;
+    final double METERS_PER_INCH = 0.0254;
+     
+    // Get user input for weight and height
+    System.out.print("Enter weight in pounds: ");
+    double weight = input.nextDouble();            // weight = 50
+    System.out.print("Enter height in inches: ");
+    double height = input.nextDouble();            // height = 100
+    
+    // Calculate Conversions
+    double weightInKilograms = weight * KILOGRAMS_PER_POUND;
+    double heightInMeters = height * METERS_PER_INCH;
 
-public class J103_1_1 { // if statement
-   public static void main(String[] args) {
-      // Create a Scanner object
-      Scanner input = new Scanner(System.in);
+    // Calculate BMI
+    double bmi = weightInKilograms / (heightInMeters * heightInMeters);
+    System.out.println("BMI is " + bmi);
 
-      // Get input for a number
-      System.out.print("Number? ");
-      int number = input.nextInt();
-
-      // Calculate and Display if input is odd or even
-      if (number % 2 == 0) {
-         System.out.println("even");
-      } else {
-         System.out.println("odd");
-      }
-
-      input.close();
-
-   }
+    // Output BMI range
+    if (bmi < 18.5)
+      System.out.println("Underweight");
+    else if (bmi < 25)
+      System.out.println("Normal");
+    else if (bmi < 30)
+      System.out.println("Overweight");
+    else
+      System.out.println("Obese");
+  }
 }
