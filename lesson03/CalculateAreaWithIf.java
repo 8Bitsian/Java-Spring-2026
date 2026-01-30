3ba2fd1471c37b07c8596d58f41e75bde6e84d77
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-01-29 19:08:24 -0500

    Refactor area calculation with if-else validation

--- lesson03/CalculateAreaWithIf.java
+++ lesson03/CalculateAreaWithIf.java
@@ -6,19 +6,19 @@ public class ComputeArea {                      // Class 'ComputerArea'
         // Create objects
         Scanner input = new Scanner(System.in); // Create Scanner object 'input'
 
-        // Declare constants
-        // final double PI = 3.14159;           // Declare constant w/final and name all caps
-
         // Get user input
         System.out.print("Enter Radius: ");     // Output prompt to console
         double radius = input.nextDouble();     // Declare variable 'radius'
 
-        // Validate with If Statement
-        if (radius > 0) {
-          // Compute the area
-          double area = Math.pow(radius, 2) * Math.PI; // Declare variable 'area' w/Math.pow() and Math.PI
+        // Validate with If-Else Statement
+        if (radius > 0) { // If TRUE output area
+          // Compute the area and declare 
+          double area = Math.pow(radius, 2) * Math.PI;
           // Output area of circle
           System.out.println("The area for " + radius + " is: " + area);
+        } else {         // Else FALSE output error message
+          System.out.println("ERROR: Invalid Input");
+          System.out.println("Enter a Positive Value Greater Than 0.");
         }
 
         // Close Scanner object 'input'
