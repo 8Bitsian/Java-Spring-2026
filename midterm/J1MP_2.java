e6fc3217ddeb892982378d38f8e58695087af78d
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-02-20 13:39:12 -0500

    Update J1MP_2.java

--- midterm/J1MP_2.java
+++ midterm/J1MP_2.java
@@ -1,6 +1,34 @@
 /* 
    Imani Hollie 02/20/2026
    Midterm Project: Part 2 - Acount Saving Checking
+   A bank in your towns updates its customers' accounts at the end of each month.
+   The bank offers two types of accounts: Savings and Checking.
+   Every customer must maintain a minimum balance or they will be penalized with a service charge as follows:
+   - $10.00 for savings
+   - $25.00 for checking
+
+   If the balance at the end of the month is at least the minimum, the accout recieves iterest as follows:
+   - 4% interest for savings
+   - Checking accounts w/balanace of up to $5,000 more than the minumum recieve 3% interest; Otherwise, the interest is 5%
+
+   Write a program that reads:
+   - a customer’s account number (int type)
+   - account type (char type; S for savings and C for checking)
+   - minimum balance that the account should maintain
+   - current balance
+
+   The program should then output:
+   - The account number
+   - Account type
+   - Current balance and new balance
+   - Or an appropriate error message
+
+   Test your program by running it five times, by using proper the following data:
+   46728 S 1000 2700 
+   87324 C 1500 7689 
+   873 S 1000 800 
+   89832 C 2000 3000 
+   98322 C 1000 750
 */
 
 // Import utility library
