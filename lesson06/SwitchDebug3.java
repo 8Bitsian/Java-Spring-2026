d03835eac3976dcf138bb1268ccd490aaee8fdd8
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-02-19 20:13:08 -0500

    Create SwitchDebug3.java

---
+++ lesson06/SwitchDebug3.java
@@ -0,0 +1,25 @@
+public class SwitchDebug3.java {
+  public static void main(String[] args) {
+    /* 
+      int grade = 2;
+      switch (grade) {
+        case 1: System.out.println("A"); // There are no break; keywords
+        case 2: System.out.println("B");
+        case 3: System.out.println("C");
+        case 4: System.out.println("D");
+        case 5: System.out.println("F");
+        default: System.out.println("Invalid");
+      }
+    */
+
+    int grade = 2;
+    switch (grade) {
+      case 1: System.out.println("A"); break;
+      case 2: System.out.println("B"); break; // Only prints "B"
+      case 3: System.out.println("C"); break;
+      case 4: System.out.println("D"); break;
+      case 5: System.out.println("F"); break;
+      default: System.out.println("Invalid");
+    }
+  }
+}
