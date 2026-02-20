04c07faa0768d667bae62ba2c0d78be3ae82cf9c
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-02-20 13:40:35 -0500

    Update J1MP_3.java

--- midterm/J1MP_3.java
+++ midterm/J1MP_3.java
@@ -1,6 +1,18 @@
 /* 
    Imani Hollie 02/20/2026
    Midterm Project: Part 3 - Parking Charges
+
+   A parking garage charges a $2.00 minimum fee to park for up to three hours.
+   The garage charges an additional $0.50 per hour for each hour or part thereof in excess of three hours.
+   The maximum charge for any given 24-hour period is $10.00.
+   Assume that no car parks for longer than 24 hours at a time.
+
+   Write an application that calculates and displays the parking charges for each customer who parked in the garage yesterday.
+   You should enter the hours parked for each customer.
+
+   The program should display:
+   - The charge for the current customer
+   - Calculate and display the running total of yesterday’s receipts
 */
 
 // Import utility library
