54e0cb466761bbfc91b5c6c8d9e486c22aaa23a4
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-02-19 20:01:37 -0500

    Create LoopDebug1.java

---
+++ lesson06/LoopDebug1.java
@@ -0,0 +1,17 @@
+// Imani Hollie 02/19/2026
+
+public class LoopDebug1 {
+  public static void main(String[] args) {
+    /*
+      // Initislizes i = 1, Checks i is greater or less than 10, Postfix decrement
+      for (int i = 1; i <= 10; i--) { // i will always be less than 10 so its a infinite loop
+        System.out.println(i);
+      }
+    */
+    
+    // Initislizes i = 1, Checks i is greater or less than 10, Postfix increment
+    for (int i = 1; i <= 10; i--) {
+      System.out.println(i); // Will print 10 since it starts at 1
+    }
+  }
+}
