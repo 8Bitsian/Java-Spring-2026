f5a528077aadafaea6b1b0c6163397af50ffa669
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-02-20 13:06:06 -0500

    Create J1MP_2.java

---
+++ midterm/J1MP_2.java
@@ -0,0 +1,22 @@
+/* 
+   Imani Hollie 02/20/2026
+   Midterm Project: Part 2 - Acount Saving Checking
+*/
+
+// Import utility library
+import java.util.Scanner; // Scanner class allows use of Scanner object
+
+public class J1MP_2 {
+  public static void main(String[] args) {
+    // Create Scanner object "input"
+    Scanner input = new Scanner(System.in);
+
+    // Declare variables
+
+    // Get user's input
+    
+    // Output
+
+    input.close();  // Close scanner
+  }
+}
