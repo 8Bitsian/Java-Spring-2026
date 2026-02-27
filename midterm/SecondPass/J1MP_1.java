9fb89afc7850efcd7e424a003f40717986753d54
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-02-27 10:40:12 -0500

    Refactor J1MP_1.java for clarity and functionality

--- midterm/SecondPass/J1MP_1.java
+++ midterm/SecondPass/J1MP_1.java
@@ -2,80 +2,83 @@
    Imani Hollie 02/20/2026
    Midterm Project: Part 1 - Calculating Sales
    An online retailer sells 5 products whose retail prices are as follows:
-  - Product 1: $2.98
-  - Product 2: $4.50
-  - Product 3: $9.98
-  - Product 4: $4.49
-  - Product 5: $6.87
+   - Product 1: $2.98
+   - Product 2: $4.50
+   - Product 3: $9.98
+   - Product 4: $4.49
+   - Product 5: $6.87
 
-  Write an application that reads a series of a pair of numbers as follows:
-  - Product Number
-  - Quantitiy Sold
+   Write an application that reads a series of a pair of numbers as follows:
+   - Product Number
+   - Quantitiy Sold
 
-  Your program should use a switch statement to determine the retail price for each product.
-  It should calculate and display the total retail value of all products sold.
-  Use a sentinel-controlled loop to determine when the program should stop looping and display the final results.
+   Your program should use a switch statement to determine the retail price for each product.
+   It should calculate and display the total retail value of all products sold.
+   Use a sentinel-controlled loop to determine when the program should stop looping and display the final results.
 */
 
 // Import utility library
 import java.util.Scanner; // Scanner class allows use of Scanner object
 
 public class J1MP_1 {
-  public static void main(String[] args) {
-    // Create Scanner object "input"
-    Scanner input = new Scanner(System.in);
+   public static void main(String[] args) { // 1. Program execution starts with main() method 
+      // Declare and initialize variable
+      double totalValue = 0.0;
 
-    // Declare variables
-    int quantitySold;
-    double retailPrice = 0.0;
-    double subtotal = 0.0;
-    double totalValue = 0.0;
+      // Prompt for user input (Sentinel Value = -1)
+      System.out.println("Enter quantity purchased for each product (-1 to EXIT):\n");
 
-    // Get user's input (Sentinel Value = -1)
-    System.out.println("Enter quantity purchased for each product (-1 to EXIT):\n");
-    
-    // Loop through product numbers 1-5
-    for (int productNumber = 1; productNumber <= 5; productNumber++) {
-      // Prompt user to enter quantity of products bought
-      System.out.printf("Enter Quantity of Product %d: ", productNumber);
-      quantitySold = input.nextInt();
+      // Loop through product numbers 1-5
+      for (int productNumber = 1; productNumber <= 5; productNumber++) {
+         // Call getQuantity() to get user input
+         int quantitySold = getQuantity(productNumber);
+         
+         // Check if user enters sentinel value, then break the for loop
+         if (quantitySold == -1) break;
 
-      // Sentinel check
-      if (quantitySold == -1) { break; }
-
-      // Calculate retail price and total value spent
-      switch (productNumber) {
-        case 1: // Product 1: $2.98
-          retailPrice = 2.98;
-          break;
-        case 2: // Product 2: $4.50
-          retailPrice = 4.50;
-          break;
-        case 3: // Product 3: $9.98
-          retailPrice = 9.98;
-          break;
-        case 4: // Product 4: $4.49
-          retailPrice = 4.49;
-          break;
-        case 5: // Product 5: $6.87
-          retailPrice = 6.87;
-          break;
-        default:
-          retailPrice = 0.0;
+         // Call getRetailPrice() to determine the retail price of each product
+         double retailPrice = getRetailPrice(productNumber);
+         
+         // Calculate subtotal and running total
+         double subtotal = retailPrice * quantitySold;
+         totalValue += subtotal;
+         
+         // Call displaySubtotal() to show the subtotal for each product
+         displaySubtotal(productNumber, subtotal);
       }
+      // Call displayTotal() to show the running total for all products
+      displayTotal(totalValue)
+      input.close();
+   }
 
-      // Calculate subtotal and total
-      subtotal = retailPrice * quantitySold;
-      totalValue += subtotal;
+   public static int getQuantity(int productNumber) { // 2. Get user input for quantity of each product
+      // Create Scanner object "input"
+      Scanner input = new Scanner(System.in);
+      // Get user input of quantity of products bought
+      System.out.printf("Enter Quantity of Product %d: ", productNumber);
+      return = input.nextInt();
+   }
+   
+   public static double getRetailPrice(int productNumber) { // 3. Determine retail price using switch
+      // Determine retail price and total value spent
+      switch (productNumber) {
+         case 1: return 2.98; // Product 1: $2.98
+         case 2: return 4.50; // Product 2: $4.50
+         case 3: return 9.98; // Product 3: $9.98
+         case 4: return 4.49; // Product 4: $4.49
+         case 5: return 6.87; // Product 5: $6.87
+         default: return 0.0;
+      }
+   }
 
+   public static void displaySubtotal(int productNumber, double subtotal) { // 3. Display subtotal for each product
       // Output subtotal for this product
       System.out.printf("Subtotal for Product %d: $%.2f%n%n", productNumber, subtotal);
-    }
-    
-    // Output total cost
-    System.out.println("=============================================");
-    System.out.printf("Total Cost: $%.2f%n", totalValue);
+   }
 
-    input.close();  // Close scanner
-  }
+   public static void displaySubtotal(double totalValue) { // 4. Display running total
+      // Output total cost
+      System.out.println("=============================================");
+      System.out.printf("Total Cost: $%.2f%n", totalValue);
+   }
 }
