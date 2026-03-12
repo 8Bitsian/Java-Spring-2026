68714b6fa5d4d082084879aecbd7318effb0a9ed
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-03-12 19:11:05 -0400

    Add PrintValues class for user input handling
    Implement PrintValues class to read and display user input.

---
+++ lesson07/Classwork/PrintValues.java
@@ -0,0 +1,20 @@
+/* Imani Hollie 03/12/2026
+   Lesson 7 Classwork
+*/
+import java.util.Scanner;
+
+public class PrintValues {
+  public static void main(String[] args) {
+    // Create Scanner object 'input'
+    Scanner input = new Scanner(System.in);
+    // Declare and initialize 'myList' array with size of 5
+    double[] myList = new double[5];
+    // Get user input
+    System.out.println("Enter " + myList.length + " values: ");
+    // input for loop and insert values into 
+    for (int i = 0; i < myList.length; i++) {
+      myList[i] = input.nextDouble();
+      System.out.println("Value " + myList.length + ": " + myList[i]);
+    }
+  }
+}
