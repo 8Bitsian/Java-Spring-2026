a9cb8c4254122a51475b31d66da15c9e98285993
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-03-13 10:21:38 -0400

    Create J107_3.java

---
+++ lesson07/J107_3.java
@@ -0,0 +1,31 @@
+/* Imani Hollie 03/13/2026
+   Lesson 7 Assignment: Utilize an array to produce an output similar to the sample output below.
+
+   The code should:
+   - prompt the user to input 10 upper- and lower-case letters
+   - use an algorithm to sort the characters in the array
+   - have comments to indicate where modifications were made to the textbook algorithm
+
+   odify the sorting and searching algorithms from the textbook, ensuring your implementation is different.
+   Avoid using any built-in methods, such as sort()
+
+   Sample output:
+   10 upper or lowercase characters? j K L e w F v m p X
+   Ascending(1) or Descending(2) order? 1
+   =============
+   Your array: j K L e w F v m p X
+   Ascending : e F j K L m p V w X
+   -----------------------------------------------------
+   10 upper or lowercase characters? j K L e w F v m p X
+   Ascending(1) or Descending(2) order? 2
+   =============
+   Your array: j K L e w F v m p X
+   Decending : X w V p m L K j F e
+   -----------------------------------------------------
+   10 upper or lowercase characters? 0
+   OK done!
+*/
+
+/* Imani Hollie 03/13/2026
+   Lesson 7 Assignment: Utilize an array to produce an output similar to the sample output below.
+*/
