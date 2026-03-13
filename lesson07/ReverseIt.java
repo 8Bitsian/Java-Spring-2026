27d355833676afbe7cb7b83912976910d2411461
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-03-12 20:48:00 -0400

    Create ReverseIt.java

---
+++ lesson07/ReverseIt.java
@@ -0,0 +1,10 @@
+/*
+  Reverse the numbers entered
+  Write a program that reads 10 integers then displays them in the reverse order in which they were read.
+
+  Sample Output:
+  Read a number: 2
+  ...
+  Read a number: 90
+  The reversal of the input is 90 ... 2
+*/
