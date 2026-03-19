e127145b02aeeabc39398fed3f1a9e78df997f08
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-03-19 17:17:51 -0400

    Create J108_1.java

---
+++ lesson08/J108_1.java
@@ -0,0 +1,15 @@
+/* Imani Hollie 03/19/2026
+   Lesson 8 Assignment: Write a program that uses a loop to initialize a @D array column-wise with the data below:
+   1 4 7 10 13 → Row-wise
+   2 5 8 11 14
+   3 6 9 12 15 ↓ Column-wise
+   
+   The code should have two methods:
+   - genArr to generate the array
+   - printArr to print out the array row-wise
+*/
+
+public class J108_1 {
+   public static void main(String[] args) {
+   }
+}
