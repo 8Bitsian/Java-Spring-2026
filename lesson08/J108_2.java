d10cc2b2a43b0459c5c062413a4e25f0dfc2745f
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-03-19 17:10:03 -0400

    Create J108_2.java

---
+++ lesson08/J108_2.java
@@ -0,0 +1,33 @@
+/* Imani Hollie 03/19/2026
+   Lesson 8 Assignment: Create a dataset to store ad calculate students' exam scores for subject A, B, C in each semester.
+
+   The code should have the following:
+   - Utilize a 3-dimensional array:
+     Name/Subject   A  B  C   A  B  C   A  B  C
+     Student1       1  2  3   4  5  6   7  8  9
+     Student2      10 11 12  13 14 15  16 17 18
+     Student3      19 20 21  22 23 24  25 26 27
+     Student4      28 29 30  31 32 33  34 35 36
+     Student5      37 38 39  40 41 42  43 44 45
+   - Allow the user to choose multiple semesters with 1 subject or 1 semester w/multiple subjects
+   - Print out the corresponding average(s) of the selected exam score(s)
+
+   Sample Output:
+   Which semester? Fall(1), Spring(2), or Summer(3)? 2
+   Which subject? A B
+   Subject  Semester  Avergage
+   A        Winter    **
+   B        Winter    **
+   Which semester? Fall(1), Spring(2), or Summer(3)? 1 2
+   Which subject? C
+   Subject  Semester  Avergage
+   C        Fall      **
+   B        Spring    **
+   Which semester? Fall(1), Spring(2), or Summer(3)? 0
+   Thank you.
+*/
+
+public class J108_2 {
+   public static void main(String[] args) {
+   }
+}
