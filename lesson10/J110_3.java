6463c8b7ef2720bb9b404c2f5d01e0cdb9b4f6a7
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-03-31 07:17:27 -0400

    Add Euler class for estimating Euler's number
    Created a new class 'Euler' to estimate Euler's number.

---
+++ lesson10/J110_3.java
@@ -0,0 +1,15 @@
+/*
+    Imani Hollie 03/31/2026
+    Lab 10 Assignment: Write a code to estimate Euler's number using the formula below:
+    - e = 1 + 1/1! + 1/2! + 1/3! + ... + 1/i! = 2.71828182845...
+    Write a code that performs the following steps:
+    - Create an "Euler" class to calculate the value of e for i = 1, 2, 5, 10, 50, 100, 200
+    - Refer to https://www.mathisfun.com/numbers/e-eulers-number.html for more information
+    - The input should be the maximum of i used in the summation
+*/
+
+public class J110_3 {
+}
+
+class Euler {
+}
