1933eca4f9e605a342abfc3b229ea90ef00dc33f
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-04-16 18:32:10 -0400

    Update LoanCalculatorWithMethod.java

--- lesson11/Classwork/LoanCalculatorWithMethod.java
+++ lesson11/Classwork/LoanCalculatorWithMethod.java
@@ -1,25 +1,31 @@
 import java.util.Scanner;
 
 public class LoanCalculatorWithMethod {
-  public staic void main(String[] args) {
-    LoanCalculatorWithMethod lc = 
+
+  public static void main(String[] args) {
+    LoanCalculatorWithMethod lc = new LoanCalculatorWithMethod();
     Scanner input = new Scanner(System.in);
 
-    System.out.print("Enter annual interest rate, e.g, 7.25% : ");
+    System.out.print("Enter annual interest rate, e.g., 7.25%: ");
     double annualRate = input.nextDouble();
-    
-    double monthlyRate = annualRate / 1200;
 
-    System.out.print("Enter number of years, e.g, 5 : ");
+    System.out.print("Enter number of years, e.g., 5: ");
     int numberOfYears = input.nextInt();
 
-    System.out.print("Enter loan amount, e.g, 12000.52 : ");
+    System.out.print("Enter loan amount, e.g., 12000.52: ");
     double loanAmount = input.nextDouble();
-    
-    calculateLoan(annualRate, numberOfYears, loanAmount);
+
+    lc.calculateLoan(annualRate, numberOfYears, loanAmount);
   }
 
   private void calculateLoan(double annualRate, int numberOfYears, double loanAmount) {
-    
+    double monthlyRate = annualRate / 1200;
+
+    double monthlyPayment = loanAmount * monthlyRate / (1 - 1 / Math.pow(1 + monthlyRate, numberOfYears * 12));
+
+    double totalPayment = monthlyPayment * numberOfYears * 12;
+
+    System.out.printf("The monthly payment is: %.2f%n", monthlyPayment);
+    System.out.printf("The total payment is: %.2f%n", totalPayment);
   }
 }
