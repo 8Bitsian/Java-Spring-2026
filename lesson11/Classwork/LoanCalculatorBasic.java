b983be81a58cbf8b8e348dc9e60654b4e1ea2aaf
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-04-16 18:25:46 -0400

    Update LoanCalculatorBasic.java

--- lesson11/Classwork/LoanCalculatorBasic.java
+++ lesson11/Classwork/LoanCalculatorBasic.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class LoanCalculator {
   public staic void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
     System.out.print("Enter annual interest rate, e.g, 7.25% : ");
     double annualRate = input.nextDouble();
     
