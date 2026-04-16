18a615623c40f0e9808983349205351767fb784e
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-04-16 18:17:26 -0400

    Create LoanCalculatorBasic.java

---
+++ lesson11/Classwork/LoanCalculatorBasic.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LoanCalculator {
+  public staic void main(String[] args) {
+    System.out.print("Enter annual interest rate, e.g, 7.25% : ");
+    double annualRate = input.nextDouble();
+    
+    double monthlyRate = annualRate / 1200;
+
+    System.out.print("Enter number of years, e.g, 5 : ");
+    int numberOfYears = input.nextInt();
+
+    System.out.print("Enter loan amount, e.g, 12000.52 : ");
+    double loanAmount = input.nextDouble();
+
+    double monthlyPayment = loanAmount * monthlyRate / (1 - 1 / Math.pow(1 + monthlyRate, numberOfYears * 12));
+
+    double totalPaymentt = monthlyPayment * numberOfYears * 12;
+
+    System.out.print("Enter loan amount, e.g, 12000.52 : ");
+    double loanAmount = input.nextDouble();
+  }
+}
