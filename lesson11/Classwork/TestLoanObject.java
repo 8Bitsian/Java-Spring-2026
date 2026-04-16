3f908582bdf86b20527951d597a52699997bb0d0
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-04-16 18:57:41 -0400

    Update TestLoanObject.java

--- lesson11/Classwork/TestLoanObject.java
+++ lesson11/Classwork/TestLoanObject.java
@@ -1,5 +1,22 @@
-public class TestLoanClass {
-  public static void main(string[] args) {
-    int num
-  }
+import java.util.Scanner;
+
+public class TestLoanObject {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter annual interest rate, e.g., 7.25%: ");
+        double annualRate = input.nextDouble();
+
+        System.out.print("Enter number of years, e.g., 5: ");
+        int numberOfYears = input.nextInt();
+
+        System.out.print("Enter loan amount, e.g., 12000.52: ");
+        double loanAmount = input.nextDouble();
+
+        LoanObject loan = new LoanObject(annualRate, numberOfYears, loanAmount);
+
+        System.out.printf("The date the loan was made is: %s%n", loan.getLoanDate());
+        System.out.printf("The monthly payment is: %.2f%n", loan.getMonthlyPayment());
+        System.out.printf("The total payment is: %.2f%n", loan.getTotalPayment());
+    }
 }
