cd53cdb4bb53525b23b9f42087296cfc253bb0f2
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-04-16 19:34:04 -0400

    Create StringDemo.java

---
+++ lesson11/Classwork/StringDemo.java
@@ -0,0 +1,13 @@
+public class StringDemo {
+    public static void main(String[] args) {
+        String m1 = new String("Java is fun!");
+        String m2 = "Java is fun!";
+        String m3 = "C++ is fun!";
+        System.out.println(m1.equals(m2));
+        System.out.println(m1.equals(m3));
+        System.out.println(m1.charAt(0));
+        System.out.println(m1.substring(5));
+        System.out.println(m1.substring(5, m1.length()));
+        System.out.println(m1.toLowerCase());
+    }
+}
