a15b914d8322c46437d7a0f2835b651fc5c0c97b
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-04-16 18:30:48 -0400

    Update LoanCalculatorBasic.java

--- lesson11/Classwork/LoanCalculatorBasic.java
+++ lesson11/Classwork/LoanCalculatorBasic.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;
 
-public class LoanCalculator {
-  public staic void main(String[] args) {
+public class LoanCalculatorBasic {
+  public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    
-    System.out.print("Enter annual interest rate, e.g, 7.25% : ");
+
+    System.out.print("Enter annual interest rate, e.g., 7.25%: ");
     double annualRate = input.nextDouble();
-    
+
     double monthlyRate = annualRate / 1200;
 
-    System.out.print("Enter number of years, e.g, 5 : ");
+    System.out.print("Enter number of years, e.g., 5: ");
     int numberOfYears = input.nextInt();
 
-    System.out.print("Enter loan amount, e.g, 12000.52 : ");
+    System.out.print("Enter loan amount, e.g., 12000.52: ");
     double loanAmount = input.nextDouble();
 
-    double monthlyPayment = loanAmount * monthlyRate / (1 - 1 / Math.pow(1 + monthlyRate, numberOfYears * 12));
+    double monthlyPayment = loanAmount * monthlyRate /
+        (1 - 1 / Math.pow(1 + monthlyRate, numberOfYears * 12));
 
     double totalPayment = monthlyPayment * numberOfYears * 12;
 
-    System.out.print("The monthly payment is : " + monthlyPayment);
-    System.out.print("The total payment is : " + totalPayment);
+    System.out.println("The monthly payment is: " + monthlyPayment);
+    System.out.println("The total payment is: " + totalPayment);
   }
 }
+
